cc9f1e32327b74316308eec2c6e5797d79d18012
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-10-21 13:50:28 -0400

    commit tree 363dd1e7dd23b7f6c3d5fa755a84f9b6e0861a6c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.collect { |name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+    new_arr = []
+    array.each_with_index do |name, index| 
+        room_num = index + 1
+        new_arr.push("Hello, #{name}! You'll be assigned to room #{room_num}!")
+    end
+    new_arr
+    
+end
+
+def printer(array)
+    badge_arr = batch_badge_creator(array)
+    room_arr = assign_rooms(array)
+    counter = 0
+    while counter < array.size do 
+        puts badge_arr[counter]
+        puts room_arr[counter]
+        counter += 1
+    end
+end
